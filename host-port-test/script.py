d323f792c1a46e63b2f570225d8359e526243257
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2024-07-09 15:48:31 +0700

    simplify host-port-test

--- host-port-test/script.py
+++ host-port-test/script.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python3
 
 import socket
-import sys
 
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("socket successfully created!")
-except socket.error as err:
-    print(f"socket creation failed with error: {err}")
-    sys.exit()
+if __name__ == "__main__":
+    domain = input("Enter domain to connect: ")
+    port = int(input("Enter port to connect: "))
 
-domain = input("Enter domain to connect: ")
-port = int(input("Enter port to connect: "))
-
-try:
-    host = socket.gethostbyname(domain)
-    print("socket successfully connected to the host!")
-except socket.gaierror:
-    print("error resolving host")
-    sys.exit()
-
-s.connect((host, port))
-print(f"successfully connected to {domain} on port {port}")
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host = socket.gethostbyname(domain)
+        s.settimeout(3)
+        s.connect((host, port))
+        print(f"successfully connected to {domain} on port {port}")
+    except socket.gaierror:
+        print("Failed resolving host")
+    except socket.error as err:
+        print(f"Failed to connect with error {err}")
+    finally:
+        s.close()
