f7c86471067f4fefe69405be9467e88f2cd9f5eb
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2024-07-10 22:49:37 +0700

    add close socket on simple-server-conn server

--- simple-server-conn/client.py
+++ simple-server-conn/client.py
@@ -2,13 +2,16 @@
 
 import socket
 
-s = socket.socket()
 host = '127.0.0.1'
 port = 3500
-s.connect((host, port))
 
-data = s.recv(1024)
+if __name__ == '__main__':
+    s = socket.socket()
 
-print(data.decode('utf-8'))
+    s.connect((host, port))
 
-s.close()
+    data = s.recv(1024)
+
+    print(data.decode('utf-8'))
+
+    s.close()
--- simple-server-conn/server.py
+++ simple-server-conn/server.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
+
 import socket
 
-s = socket.socket()
-s.bind(('', 3500))
-s.listen(5)
-print("server is listening on port 3500")
+host = ''
+port = 3500
 
-while True:
-    c, addr = s.accept()
-    print(f"connected to {addr}")
-    msg = "successfully connected to server on port 3500"
-    c.send(msg.encode('utf-8'))
-    c.close()
+if __name__ == '__main__':
+    s = socket.socket()
+
+    s.bind((host, port))
+
+    s.listen(5)
+
+    print(f'server is listening on port {port}')
+
+    try:
+        while True:
+            conn, addr = s.accept()
+            print(f"connected with {addr}")
+            msg = f"you are successfully connected with server 127.0.0.1:{port}"
+            conn.send(msg.encode('utf-8'))
+            conn.close()
+    finally:
+        print('socket closing...')
+        s.close()
