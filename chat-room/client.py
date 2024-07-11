e9a9881ee8742a2072174b6c75f66cd67fd42c53
bazoka-kaka <yehezkielwiradhika@gmail.com>
2024-07-11 10:11:00 +0700

    fix chat-room bugs

--- chat-room/client.py
+++ chat-room/client.py
@@ -1,36 +1,41 @@
 #!/usr/bin/env python3
 
-import threading
 import socket
+import threading
 
 host = '127.0.0.1'
 port = 3500
+alias = input('enter your alias: ')
 
-alias = input('Enter your alias: ')
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((host, port))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
 
-def client_receive():
-    while True:
-        try:
-            msg = client.recv(1024).decode('utf-8')
-            if msg == 'alias?':
-                client.send(alias.encode('utf-8'))
-            else:
-                print(msg)
-        except:
-            print('Error!')
-            client.close()
-            break
+def receive():
+  while True:
+    try:
+      msg = s.recv(1024).decode('utf-8')
+      if msg == 'alias?':
+        s.send(alias.encode('utf-8'))
+      else:
+        print(msg)
+    except socket.error as err:
+      print(f"an error occured: {err}")
+      s.close()
+      break
 
+def send():
+  while True:
+    try:
+      msg = input("")
+      msg_full = f"{alias}: {msg}"
+      s.send(msg_full.encode('utf-8'))
+    except socket.error as err:
+      print(f"an error occured: {err}")
+      s.close()
+      break
 
-def client_send():
-    while True:
-        msg = f"{alias}: {input('')}"
-        client.send(msg.encode('utf-8'))
-
-receive_thread = threading.Thread(target = client_receive)
-receive_thread.start()
-
-send_thread = threading.Thread(target = client_send)
+send_thread = threading.Thread(target=send)
 send_thread.start()
+
+recv_thread = threading.Thread(target=receive)
+recv_thread.start()
--- chat-room/server.py
+++ chat-room/server.py
@@ -1,49 +1,53 @@
-import threading
+#!/usr/bin/env python3
+
 import socket
+import threading
 
 host = '127.0.0.1'
 port = 3500
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+s.listen(2)
 
 clients = []
 aliases = []
 
 def broadcast(msg):
-    for client in clients:
-        client.send(msg)
+  for client in clients:
+    client.send(msg)
 
 def handle_client(client):
-    while True:
-        try:
-            msg = client.recv(1024)
-            broadcast(msg)
-        except:
-            idx = clients.index(client)
-            clients.remove(client)
-            client.close()
-            alias = aliases[idx]
-            broadcast(f"{alias} has left the chat room!".encode('utf-8'))
-            aliases.remove(alias)
-            break
+  while True:
+    try:
+      msg = client.recv(1024)
+      broadcast(msg)
+    except:
+      idx = clients.index(idx)
+      clients.remove(client)
+      client.close()
+      alias = aliases[idx]
+      msg = f"{alias} left the chatroom"
+      print(msg)
+      broadcast(msg.encode('utf-8'))
+      client.send("you left the chatroom!".encode("utf-8"))
+      aliases.remove(alias)
+      break
 
-# main function to receive clients conns
 def receive():
-    while True:
-        print("Server is listening on port 3500")
-        client, addr = server.accept()
-        print(f"connection is established with {str(addr)}")
-        client.send("alias?".encode('utf-8'))
-        alias = client.recv(1024)
-        aliases.append(alias)
-        clients.append(client)
-        print(f"Alias of this client is {alias}".encode('utf-8'))
-        broadcast(f"{alias} has connected to the chat room".encode('utf-8'))
-        client.send("You are now connected!".encode('utf-8'))
-        thread = threading.Thread(target=handle_client, args=(client,))
-        thread.start()
-
-if __name__ == '__main__':
-    receive()
+  while True:
+    print(f"server is listening on port {port}")
+    conn, addr = s.accept()
+    print(f"connection established with {str(addr)}")
+    conn.send("alias?".encode('utf-8'))
+    alias = conn.recv(1024).decode('utf-8')
+    print(f"alias of the client: {alias}")
+    broadcast(f"{alias} joined the chatroom".encode('utf-8'))
+    conn.send("successfully connected to the chatroom".encode('utf-8'))
+    clients.append(conn)
+    aliases.append(alias)
+    thread = threading.Thread(target=handle_client, args=(conn,))
+    thread.start()
+    
+if __name__ == "__main__":
+  receive()
